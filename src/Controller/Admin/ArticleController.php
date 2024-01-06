7bad1ba3e5709c66f8debed2efab1f23caf8af96
matveevvv <gleb2003m@mail.ru>
2024-01-06 17:27:56 +0700

    add admin panel

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/article')]
+#[Route('/admin/article')]
 class ArticleController extends AbstractController
 {
     #[Route('/', name: 'app_article_index', methods: ['GET'])]
